9ba27fe5c72757a3d5a9eadb713a38c1c7654b16
pafou <pfoucher@gmail.com>
2025-11-10 12:06:53 +0100

    renew

---
+++ pdc/back/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Plan de charge');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
